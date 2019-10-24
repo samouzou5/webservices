b180539a671024fea2261a59e14c1feba6df19d3
Steevin <steevinamouzou@hotmail.fr>
2019-10-24 17:50:44 +0200

    Run server ok

---
+++ workshops/soap/src/main/java/org/polytech/BookEndpoint.java
@@ -0,0 +1,30 @@
+package org.polytech;
+
+import io.spring.guides.gs_producing_web_service.GetBookRequest;
+import io.spring.guides.gs_producing_web_service.GetBookResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class BookEndpoint {
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookEndpoint(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getBookRequest")
+    @ResponsePayload
+    public GetBookResponse getBook(@RequestPayload GetBookRequest request) {
+        GetBookResponse response = new GetBookResponse();
+        response.setBook(bookRepository.findBook(request.getId()));
+
+        return response;
+    }
+}
